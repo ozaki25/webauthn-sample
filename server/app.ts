0783cc8bc8143e467dd6f9f67ce5d8678eb0ab64
ozaki <fs65ugumori@yahoo.co.jp>
2019-03-20 07:34:19 +0900

    add serverless config

--- server/app.ts
+++ server/app.ts
@@ -1,3 +1,4 @@
+const serverless = require('serverless-http');
 const express = require('express');
 
 const app = express();
@@ -19,3 +20,5 @@ const port = '8080';
 app.listen(port, () => {
   console.log(`app start listening on port ${port}`);
 });
+
+module.exports.handler = serverless(app);
